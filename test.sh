862a061373c7f497050ccb2c399fe312c90af941
Richard Russon <rich@flatcap.org>
2017-11-16 20:36:25 +0000

    tidy

--- test.sh
+++ test.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
 echo -en 'travis_fold:start:apple\r'
-
+echo 'APPLE'
 for i in {1..20}; do
 	echo $i
 done
-
 echo -en 'travis_fold:end:apple\r'
-echo -en 'travis_fold:start:banana\r'
 
+echo -en 'travis_fold:start:banana\r'
+echo 'BANANA'
 for i in {21..40}; do
 	echo $i
 done
-
 echo -en 'travis_fold:end:banana\r'
-echo -en 'travis_fold:start:cherry\r'
 
+echo -en 'travis_fold:start:cherry\r'
+echo 'CHERRY'
 for i in {41..60}; do
 	echo $i
 done
-
 echo -en 'travis_fold:end:cherry\r'
 
