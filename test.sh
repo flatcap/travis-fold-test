cc7870b0bb9cd460a4e606067d4e45221b9a199a
Richard Russon <rich@flatcap.org>
2017-11-16 20:30:29 +0000

    fold

--- test.sh
+++ test.sh
@@ -3,3 +3,16 @@
 for i in {1..20}; do
 	echo $i
 done
+
+echo -en 'travis_fold:start:apple\r'
+
+for i in {21..40}; do
+	echo $i
+done
+
+echo -en 'travis_fold:end:apple\r'
+
+for i in {41..60}; do
+	echo $i
+done
+
